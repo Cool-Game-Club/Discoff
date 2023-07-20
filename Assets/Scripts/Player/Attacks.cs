9034fc052133eeb20f98349f08f174278bfb887f
Bouyou34 <34380414+Bouyou34@users.noreply.github.com>
2023-07-21 00:19:10 +0200

    add the attack script (idk if ill even use it tbh)

---
+++ Assets/Scripts/Player/Attacks.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoolGameClub
+{
+    public class Attacks : MonoBehaviour
+    {
+
+
+        void Start() {
+        
+        }
+
+        void Update() {
+        
+        }
+    }
+}
