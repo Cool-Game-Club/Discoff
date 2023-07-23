dd1c45380f3d16b5fbd732e62c11e07b0c3b7901
Bouyou34 <34380414+Bouyou34@users.noreply.github.com>
2023-07-23 14:55:57 +0200

    add the attacks script

--- Assets/Scripts/Player/Attacks.cs
+++ Assets/Scripts/Player/Attacks.cs
@@ -2,18 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CoolGameClub
+namespace CoolGameClub.Player
 {
     public class Attacks : MonoBehaviour
     {
-
-
-        void Start() {
+        [Header("Attacks Prefabs")]
+        [SerializeField] private GameObject _ringAttack; // green
+        [SerializeField] private GameObject _beamAttack; // red
+        [SerializeField] private GameObject _lightningAttack; // blue
         
+        [SerializeField] private Vector3 _attackDirection;
+
+        private void GreenAttack(float x, float y) {
+
+            Vector3 normalizedAttack = new Vector3(x, y).normalized;
+            float angle = Mathf.Atan2(normalizedAttack.y, normalizedAttack.x) * Mathf.Rad2Deg;
+            
+            GameObject attack = Instantiate(_ringAttack, transform.position, UnityEngine.Quaternion.Euler(0f, 0f, angle));
         }
 
-        void Update() {
-        
+        private void RedAttack(float x, float y) {
+
+            Vector3 normalizedAttack = new Vector3(x, y).normalized;
+            float angle = Mathf.Atan2(normalizedAttack.y, normalizedAttack.x) * Mathf.Rad2Deg;
+            
+            GameObject attack = Instantiate(_beamAttack, transform.position, UnityEngine.Quaternion.Euler(0f, 0f, angle));
         }
+
+        private void BlueAttack(float x, float y) {
+            // Make it so it targets the closest enemy.
+        }
+        /*private void PurpleAttack(float x, float y) {
+
+        }*/
+
     }
 }
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -7,33 +7,29 @@ using Vector3 = UnityEngine.Vector3;
 
 namespace CoolGameClub.Player
 {
-    public class PlayerController : MonoBehaviour
+    public class PlayerScript : MonoBehaviour
     {
         [Header("Player Speed")]
         [Tooltip("Max move speed of the character in m/s")]
         [SerializeField] private float _maxMoveSpeed = 7f;
 
-        [Header("Attacks Prefabs")] 
-        [SerializeField] private GameObject _testObject;
-        [SerializeField] private GameObject _ringAttack;
-        [SerializeField] private GameObject _beamAttack, _lightningAttack;
+        [Header("Player Attacks")] 
+        [SerializeField] private float _attackSpeed = 1.0f;
 
-        [SerializeField] private Vector3 _attackDirection;
-
-        //private PlayerInput _playerInput;
         private PlayerInputManager _input;
         private Rigidbody2D _rigidbody;
-        private Rigidbody2D _testRigidbody;
+
+        // Initializing the "nextAttack" variable, which is a float to store the time spent between attacks
+        private float _nextAttack;
 
         void Start() {
             _input = FindAnyObjectByType<PlayerInputManager>();
             _rigidbody = GetComponent<Rigidbody2D>();
-            _testRigidbody = _testObject.GetComponent<Rigidbody2D>();
         }
 
         private void FixedUpdate() {
             Move();
-            Attacks();
+            Attack(-_input.AttackDirection.x, -_input.AttackDirection.y);
         }
 
         private void Move() {
@@ -43,42 +39,18 @@ namespace CoolGameClub.Player
             // Find the change in position
             Vector3 velocity = targetSpeed * Time.fixedDeltaTime * _input.MoveDirection.normalized;
 
+            // Apply the movement changes
             _rigidbody.MovePosition(_rigidbody.transform.position + velocity);
         }
 
-        private void Attacks() {
-            // Trying method 1: didn't work. I'm gonna do a different approach anyway as it'll be more balanced between controller and keyboard. The one i tried here made it so joystick would be way better at aiming.
-            //_attackDirection = _input.AttackDirection.y + _input.AttackDirection.x;
-            //_testObject.transform.Rotate(0,0,_attackDirection);
+        private void Attack(float x, float y) {
 
-            // Didn't work either.
-            //Vector3 _attackDirection = _input.AttackDirection.normalized;
-            //_testRigidbody.MoveRotation(_testRigidbody.transform.rotation + _attackDirection);
+            if ((x == 0 && y == 0) || Time.time < _nextAttack)
+                return;
 
-            // Much more simple. I think i'm going to rename the current method (Attacks) to "Attack(direction float)" so I can generalize all the attacks to the same method and just use a different sprite depending on floor color. Will see later. (2 hours later: Idk how im going to do but i want to figure it out without googling)
-            // I did this but it turns the player too, it looks pretty funny lmao
-            _attackDirection = _input.AttackDirection;
+            _nextAttack = Time.time + _attackSpeed;
 
-            switch (_attackDirection.y) {
-                case 1:
-                    // Up
-                    _testRigidbody.SetRotation(-90);
-                    break;
-                case -1:
-                    // Down
-                    _testRigidbody.SetRotation(90);
-                    break;
-            }
-            switch (_attackDirection.x) {
-                case 1:
-                    // Right
-                    _testRigidbody.SetRotation(180);
-                    break;
-                case -1:
-                    // Left
-                    _testRigidbody.SetRotation(0);
-                    break;
-            }
+            Debug.Log("Attacking");
         }
     }
 }
